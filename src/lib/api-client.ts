a8d27b5d4397befee1331538909ec5ccce8f4587
Brian Okechi <bryankechy@gmail.com>
2026-02-28 07:09:48 +0300

    Fix API base URL by removing port number

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // const API_BASE_URL = 'http://localhost:8002/api/v1'
 
-const API_BASE_URL = 'https://sandbox.acensure.acentriagroup.com:8005/api/v1/'
+const API_BASE_URL = 'https://sandbox.acensure.acentriagroup.com/api/v1/'
 
 
 const apiClient = axios.create({
