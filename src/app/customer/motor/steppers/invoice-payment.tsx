e8b85c5ee3be8a40f814b49733eb5d96ef0514d9
evanskosgei <evanskosgei090@gmail.com>
2026-04-27 09:57:12 +0300

    some updates

--- src/app/customer/motor/steppers/invoice-payment.tsx
+++ src/app/customer/motor/steppers/invoice-payment.tsx
@@ -103,8 +103,8 @@ export const InvoicePayment: React.FC<CustomerVerificationDetailsProps> = ({ goT
                         type="submit"
                         className="w-full sm:w-auto bg-[#C20C0C]/80 rounded-full hover:bg-[#C20C0C]"
                         rightIcon={<ArrowRightCircle />}
-                        // onClick={() => goToNextStep?.()}
-                        loading={submitMutation.isPending}>
+                        onClick={() => goToNextStep?.()}>
+                        {/* loading={submitMutation.isPending}> */}
                         Complete Payment
                     </Button>
                 </div>
--- src/app/customer/motor/steppers/payment-tabs/mpesa.tsx
+++ src/app/customer/motor/steppers/payment-tabs/mpesa.tsx
@@ -22,15 +22,14 @@ export const MpesaPageTab: React.FC = () => {
     }
 
     return (
-        <div className='w-full max-w-6xl mx-auto p-1'>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-center">
+        <div className='w-full max-w- mx-auto p-1'>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-start">
                 <div className="w-full p-2 rounded-[20px] border border-[#ADABAB]/30 bg-white shadow-sm">
                     <FieldGroup>
                         <div className="mb-6 p-4 h-auto min-h-8.5 rounded-[15px] border border-[#ADABAB]/70 bg-green-50/30">
                             <span className="text-sm text-muted-foreground uppercase tracking-wider">Total to Pay:</span>
                             <h1 className='text-[#0CC258] font-bold text-2xl sm:text-2xl'>Ksh {formattedAmount}</h1>
                         </div>
-
                         <Controller
                             name="phone_number"
                             control={control}
@@ -44,6 +43,7 @@ export const MpesaPageTab: React.FC = () => {
                                         type="tel"
                                         inputMode="numeric"
                                         placeholder="0712345678"
+                                        autoComplete='off'
                                         className={`w-full h-12 rounded-xl border border-[#ADABAB] ${fieldState.invalid ? 'border-red-500 ring-1 ring-red-500' : 'focus:ring-[#0CC258]'
                                             }`}
                                         onChange={(e) => {
@@ -59,7 +59,6 @@ export const MpesaPageTab: React.FC = () => {
                                 </Field>
                             )}
                         />
-
                         <div className="mt-6 flex items-start gap-3 p-2 bg-blue-50 rounded-lg border border-blue-100">
                             <div className="mt-1 text-blue-500">
                                 <CircleAlert />
@@ -71,7 +70,7 @@ export const MpesaPageTab: React.FC = () => {
                     </FieldGroup>
                 </div>
                 
-                {/* <div className="w-full p-6 bg-gray-50 rounded-[20px] border border-dashed border-[#ADABAB]">
+                <div className="w-full p-6 bg-gray-50 rounded-[20px] border border-dashed border-[#ADABAB]">
                     <span className="font-bold text-gray-700 text-sm sm:text-base block mb-4 underline decoration-[#0CC258] underline-offset-4">
                         Option 2: Pay via Lipa na M-Pesa
                     </span>
@@ -85,7 +84,7 @@ export const MpesaPageTab: React.FC = () => {
                         <li className="pl-2">Enter your <span className="font-semibold">PIN</span> and press OK</li>
                         <li className="pl-2">Wait for the confirmation SMS</li>
                     </ol>
-                </div> */}
+                </div>
 
             </div>
         </div>
