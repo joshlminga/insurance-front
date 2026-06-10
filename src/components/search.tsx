29e61ebf3006ca81a7a30db2a6bcd9d18b145133
evanskosgei <evanskosgei090@gmail.com>
2026-06-10 12:14:46 +0300

    feat: add 404 error page component

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -4,12 +4,12 @@ import { Input } from "@/components/ui/input"
 
 export function Search() {
   return (
-    <div className="relative w-full max-w-sm">
+    <div className="relative w-full max-w-sm hidden sm:block">
       <SearchIcon className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
       <Input
         type="search"
         placeholder="Search..."
-        className="w-full bg-background pl-8 md:w-[300px] lg:w-[300px]"
+        className="w-full bg-background pl-8 md:w-75 lg:w-75"
       />
     </div>
   )
---
+++ src/errors/4o4page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Error4o4Page = () => {
+  return (
+    <div>4o4page</div>
+  )
+}
+
+export default Error4o4Page
