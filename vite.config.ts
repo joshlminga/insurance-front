8e1cd6edf3aa96abbfd836bdced5956154de0f17
Josh L. Minga <joshlminga@gmail.com>
2026-07-22 12:25:22 +0300

    Remove server settings from vite.config.ts
    Removed server configuration from Vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -9,13 +9,5 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    strictPort: false,
-    allowedHosts: [
-      '*'
-    ]
   }
 })
